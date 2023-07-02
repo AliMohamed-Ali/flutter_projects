11a74cb9efe0503caa7a6aa459f72ea844c1941c
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-07-02 10:24:14 +0300

    create custom list view item

--- bookly_app/lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ bookly_app/lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
@@ -10,7 +11,31 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
+    );
+  }
+}
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: AspectRatio(
+        aspectRatio: 2.6 / 3.9,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.red,
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsData.testImage),
+              )),
+        ),
+      ),
     );
   }
 }
