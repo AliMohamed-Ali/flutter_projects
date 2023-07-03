edb0f8ca505f22e1a91d0dacd3546c0384b7f717
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-07-03 18:39:25 +0300

    create search view page

--- bookly_app/lib/core/utils/app_routes.dart
+++ bookly_app/lib/core/utils/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/Home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/Splash/presentation/views/splash_view.dart';
@@ -8,12 +9,19 @@ abstract class AppRouter
 {
   static const kHomeView = "/homeView";
   static const kBookDetailsView = "/bookDetailsView";
+  static const kSearchView = "/searchView";
   static final GoRouter router = GoRouter(
   routes: <RouteBase>[
      GoRoute(
       path: '/',
       builder: ( context,  state) {
         return const SplashView();
+      },
+    ),
+    GoRoute(
+      path: kSearchView,
+      builder: ( context,  state) {
+        return const SearchView();
       },
     ),
     GoRoute(
--- bookly_app/lib/features/Home/presentation/views/widgets/custom_app_bar.dart
+++ bookly_app/lib/features/Home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_routes.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -8,8 +10,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:
-          const EdgeInsets.only( top: 40, bottom: 20),
+      padding: const EdgeInsets.only(top: 40, bottom: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -18,7 +19,9 @@ class CustomAppBar extends StatelessWidget {
             height: 18,
           ),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kSearchView);
+              },
               icon: const Icon(
                 FontAwesomeIcons.magnifyingGlass,
                 size: 24,
