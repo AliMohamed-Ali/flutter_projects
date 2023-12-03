914535d730dea1deecf3be454a9fcd50164ba4e6
Ali Mohamed <61457710+AliMohamed-Ali@users.noreply.github.com>
2023-12-03 15:54:26 +0200

    Delete test_app directory

--- test_app/lib/controller/home_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class Controller extends GetxController {
-  // var count = 0.obs;
-  var count1 = 0.obs;
-  var count2 = 0.obs;
-  increment() => count2++;
-  int get sum => count1.value + count2.value;
-}
--- test_app/lib/locale/locale_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class MyLocaleController extends GetxController {
-  void changeLang (String codeLang){
-    Locale locale = Locale(codeLang);
-    Get.updateLocale(locale);
-  }
-}
